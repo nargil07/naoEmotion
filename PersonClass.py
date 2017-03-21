6fed5ef771288731a76d5babe10d2ca70377250c
Slyclouds <slyclouds@hotmail.fr>
2017-03-21 15:36:06 +0100

    Json Interpreter

---
+++ PersonClass.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Imports
+import json
+import numpy as np
+
+# Class code
+
+
+class Person:
+    """
+
+    """
+    emotions = "unknown";
+
+    def getHumor(self, emotions) :
+        dominantEmotion = "unknown"
+
+
+        return dominantEmotion
+
+    def __init__(self, personParms):
+
+        faceRectangle = {}
+        faceRectangle['left'] = personParms["faceRectangle"]
+
+        emotions = personParms["scores"]
---
+++ jsonInterpreter.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Imports
+import json
+import numpy as np
+from PersonClass import Person
+#
+def addPeople(crosoftJson) :
+    elJson = json.loads(crosoftJson)
+    gens = []
+
+    for x in elJson:
+        gens.append(Person(x))
