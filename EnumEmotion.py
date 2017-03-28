191c4527336207b2f4df8be03ce29261ddf1d509
nargil07 <nargilcorp@gmail.com>
2017-03-28 14:04:40 +0200

    ajout de l'enum des emotions

---
+++ EnumEmotion.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class EnumEmotion(Enum):
+    NEUTRAL = "neutral"
+    ANGER = "anger"
+    CONTEMPT = "contempt"
+    DISGUST = "disgust"
+    FEAR = "fear"
+    HAPPINESS = "happiness"
+    SADNESS = "sadness"
+    SURPRISE = "surprise"
